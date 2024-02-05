693bfbaf32ed919ae6c76a6cd14995a41274ac5b
Ky0sann <mathis.huard@supdevinci-edu.fr>
2024-02-05 19:11:19 +0100

    Généralisation de la fonction "request_async" et "display" dans app.js
    Le fichier app.js (et aussi search.html en conséquence) a été modifié pour que les fonctions précédemment "request_async_pikachu" et "display_pikachu" deviennent des fonctions générales, c'est à dire des fonctions qui peuvent être appellées pour n'importe lequel des 151 premiers pokemons (grâce au paramètre d'entrée "number" correspondant aux numéros du pokemon, comme 25 pour Pikachu). Pour l'instant, le tout ne marche que avec pikachu (en tapant "pikachu" dans la barre de recherche).

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-async function request_async_pikachu() { // fonction asynchrone pour aller chercher les données du pokemon pikachu
+async function request_async_pokemon(number) { // fonction asynchrone pour aller chercher les données du pokemon
   try { 
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu'); // on attend la réponse du lien vers le pokemon
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`); // on attend la réponse du lien vers le pokemon
     const data = await response.json(); // on attend la conversion de la réponse en json
     
     return data; 
 
   } catch (error) {
-    console.error('Erreur lors de la récupération des données de Pikachu :', error); // on affiche l'erreur si il y en a une
+    console.error('Erreur lors de la récupération des données du pokemon :', error); // on affiche l'erreur si il y en a une
   }
 }
 
@@ -16,7 +16,7 @@ async function request_async_pikachu() { // fonction asynchrone pour aller cherc
 .catch(error => console.log(error));
 */
 
-async function request_async_region() { // fonction asynchrone pour aller chercher les données du pokemon pikachu
+/* async function request_async_region() { // fonction asynchrone pour aller chercher les données du pokemon pikachu
   try { 
     const response = await fetch('https://pokeapi.co/api/v2/region/'); // on attend la réponse du lien vers les régions
     const data = await response.json(); // on attend la conversion de la réponse en json
@@ -24,22 +24,22 @@ async function request_async_region() { // fonction asynchrone pour aller cherch
   } catch (error) {
     console.error('Erreur lors de la récupération des données de Region :', error); // on affiche l'erreur si il y en a une
   }
-}
+} */
 
-async function pikachu_display() {
-  const pikachu_info = document.getElementById('pikachu-info'); // on récupère l'élément html où on va afficher les données
-  const region_info = document.getElementById('region-info'); // on récupère l'élément html où on va afficher les données
-  const region_data = await request_async_region(); // on attend les données de la région
-  const pikachu_data = await request_async_pikachu(); // on attend les données du pokemon pikachu
-  const moves = pikachu_data.moves.map(move => move.move.name); // on récupère les noms des mouvements du pokemon
-  const types = pikachu_data.types.map(type => type.type.name); // on récupère les types du pokemon
-  const stat_name = pikachu_data.stats.map(stat => stat.stat.name); // on récupère les noms des stats du pokemon
-  const stat = pikachu_data.stats.map(stat => stat.base_stat); // on récupère les stats du pokemon
+async function pokemon_display(number) {
+  const pokemon_info = document.getElementById('pokemon-info'); // on récupère l'élément html où on va afficher les données
+  // const region_info = document.getElementById('region-info'); // on récupère l'élément html où on va afficher les données
+  // const region_data = await request_async_region(); // on attend les données de la région
+  const pokemon_data = await request_async_pokemon(number); // on attend les données du pokemon
+  const moves = pokemon_data.moves.map(move => move.move.name); // on récupère les noms des mouvements du pokemon
+  const types = pokemon_data.types.map(type => type.type.name); // on récupère les types du pokemon
+  const stat_name = pokemon_data.stats.map(stat => stat.stat.name); // on récupère les noms des stats du pokemon
+  const stat = pokemon_data.stats.map(stat => stat.base_stat); // on récupère les stats du pokemon
 
-  const sprite = `<p1><img src="${pikachu_data.sprites.front_default}"></p1><br>`;
-  const basicInfo = `<p2>Nom : ${pikachu_data.name}</p2><br>
-                    <p3>Taille : ${pikachu_data.height} dm</p3><br>
-                    <p4>Poids : ${pikachu_data.weight} hg</p4><br>
+  const sprite = `<p1><img src="${pokemon_data.sprites.front_default}"></p1><br>`;
+  const basicInfo = `<p2>Nom : ${pokemon_data.name}</p2><br>
+                    <p3>Taille : ${pokemon_data.height} dm</p3><br>
+                    <p4>Poids : ${pokemon_data.weight} hg</p4><br>
                     <p5>Types : ${types.join(', ')}</p5>`;
   
   const sortmoves = moves.sort();
@@ -53,7 +53,7 @@ async function pikachu_display() {
   }
 
   const info = `${sprite}${basicInfo}<br><br>${attacks}<br><br>${statsInfo}`; 
-  pikachu_info.innerHTML = info; // on affiche les données dans l'élément html
+  pokemon_info.innerHTML = info; // on affiche les données dans l'élément html
 }
 
 
@@ -73,11 +73,11 @@ async function simulateSearch(searchTerm) {
  
   var result = "Aucun résultat correspondant.";
 
-  // Ajouter une condition spécifique pour "pikachu"
+  // Ajout  d'une condition spécifique pour "pikachu"
   if (searchTerm.toLowerCase().includes("pikachu")) {
       try {
           // Attendre que la promesse soit résolue avant de récupérer les résultats
-          await pikachu_display();
+          await pokemon_display(25);
           result = "Affichage des informations de Pikachu réussi.";
           final_result.style.display = "block";
       } catch (error) {
@@ -86,8 +86,9 @@ async function simulateSearch(searchTerm) {
       }
   }
 
-  // Afficher le résultat dans l'élément approprié
+  // Affichage du résultat dans l'élément approprié (avec innerHTML)
   final_result.innerHTML = result;
 
 
 }
+
